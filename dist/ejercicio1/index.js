b08974dcc626621990345f7cbb16c54c2d462a18
JhoanS5 <JhoanS5.Dev@gmail.com>
2025-11-21 05:14:30 -0500

    Eliminar dist del repositorio

--- dist/ejercicio1/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const analizarLista_1 = require("./analizarLista");
-const valores = [12, 10, 5, 20];
-console.log((0, analizarLista_1.analizarLista)(valores));
-//# sourceMappingURL=index.js.map
--- dist/ejercicio3/data/usuarios.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.usuariosRegistrados = void 0;
-exports.usuariosRegistrados = [
-    { email: "admin@gmail.com", password: "1234", nombre: "Administrador" },
-    { email: "cliente@gmail.com", password: "1234", nombre: "Jhoan" }
-];
-//# sourceMappingURL=usuarios.js.map
